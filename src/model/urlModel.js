19ff6ade8a6aef8acec946ad9efd89a51f62adc7
Saurav438c <saurav438c@gmail.com>
2022-09-29 11:59:09 +0530

    asdf

---
+++ src/model/urlModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const urlSchema = new mongoose.Schema({
+
+    urlCode: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    longUrl: {
+        type: String,
+        required:true, 
+        trim: true
+    },
+    shortUrl: {
+        type: String,
+        unique: true,
+        required: true
+
+    }
+
+
+}, { timestamps: true })
+
+module.exports = mongoose.model("URL", urlSchema)
